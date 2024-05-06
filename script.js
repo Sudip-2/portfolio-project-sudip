53c74464ad3bf42bbc368c20c91030a215019ce5
pritamstech <pritamstech@hotmail.com>
2024-05-06 09:03:28 +0530

    Frontend Js Added

---
+++ script.js
@@ -0,0 +1,23 @@
+async function SendMail() {
+  let First_Name = document.querySelector("#fname").value;
+  let Email = document.querySelector("#femail").value;
+  let User_Message = document.querySelector("#usermessage").value;
+  console.log(First_Name, Email, User_Message);
+  fetch("", {
+    method: "POST",
+    body: {
+      name: First_Name,
+      email: Email,
+      message: User_Message,
+    },
+  })
+    .then((result) => {
+      return result.text();
+    })
+    .then((Final_Result) => {
+      alert(Final_Result);
+    })
+    .catch((Error) => {
+      alert(Error);
+    });
+}
