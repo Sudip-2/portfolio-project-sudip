7bc3e9a82511cfd31cbab13a04b1aa7f56794838
pritamstech <pritamstech@hotmail.com>
2024-05-06 15:33:13 +0530

    Ready To Create A Pull Request

--- script.js
+++ script.js
@@ -2,22 +2,31 @@ async function SendMail() {
   let First_Name = document.querySelector("#fname").value;
   let Email = document.querySelector("#femail").value;
   let User_Message = document.querySelector("#usermessage").value;
-  console.log(First_Name, Email, User_Message);
-  fetch("https://main--clever-longma-9048d0.netlify.app/", {
-    method: "POST",
-    body: {
-      name: First_Name,
-      email: Email,
-      message: User_Message,
-    },
-  })
-    .then((result) => {
-      return result.text();
-    })
-    .then((Final_Result) => {
-      alert(Final_Result);
-    })
-    .catch((Error) => {
-      alert(Error);
-    });
+  // console.log(First_Name, Email, User_Message);
+  let Body_Object = {
+    name: First_Name,
+    email: Email,
+    message: User_Message,
+  };
+  let result = await fetch(
+    "https://main--clever-longma-9048d0.netlify.app/submit",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(Body_Object),
+    }
+  );
+  let Final_Result = await result.text();
+  alert(Final_Result);
+  // .then((result) => {
+  //   return result.text();
+  // })
+  // .then((Final_Result) => {
+  //   alert(Final_Result);
+  // })
+  // .catch((Error) => {
+  //   alert(Error);
+  // });
 }
