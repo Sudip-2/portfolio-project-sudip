9b3cc026e008016b278862b5651e8c65188d096e
pritamstech <pritamstech@hotmail.com>
2024-05-06 15:33:40 +0530

    Ready To Create A Pull Request.

--- script.js
+++ script.js
@@ -3,30 +3,25 @@ async function SendMail() {
   let Email = document.querySelector("#femail").value;
   let User_Message = document.querySelector("#usermessage").value;
   // console.log(First_Name, Email, User_Message);
-  let Body_Object = {
-    name: First_Name,
-    email: Email,
-    message: User_Message,
-  };
-  let result = await fetch(
-    "https://main--clever-longma-9048d0.netlify.app/submit",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(Body_Object),
-    }
-  );
-  let Final_Result = await result.text();
-  alert(Final_Result);
-  // .then((result) => {
-  //   return result.text();
-  // })
-  // .then((Final_Result) => {
-  //   alert(Final_Result);
-  // })
-  // .catch((Error) => {
-  //   alert(Error);
-  // });
+  if (First_Name && Email && User_Message) {
+    let Body_Object = {
+      name: First_Name,
+      email: Email,
+      message: User_Message,
+    };
+    let result = await fetch(
+      "https://main--clever-longma-9048d0.netlify.app/submit",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(Body_Object),
+      }
+    );
+    let Final_Result = await result.text();
+    alert(Final_Result);
+  } else {
+    alert("Please Enter Proper Values To The Fields.");
+  }
 }
